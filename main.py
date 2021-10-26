59f15fecbb81f94f8bab0b58d57af2bcba5a0dbd
wpalafox <wpalafox@gmail.com>
2021-10-26 11:46:02 -0600

    Able to handle one type of format

--- main.py
+++ main.py
@@ -18,10 +18,11 @@
 #session_requests = requests.session()
 #result = session_requests.get(login_url)
 
+import selenium
 from selenium import webdriver
 
 import credentials
-import selenium
+
 import os
 import time
 import pandas as pd
@@ -73,7 +74,7 @@ button = driver.find_element_by_class_name("btn-primary")
 
 # clicking on the button
 button.click()
-time.sleep(3)
+time.sleep(2)
 
 
 code_phone = int(input("Input the code sent to your phone: "))
@@ -81,11 +82,10 @@ input_field_code = driver.find_element_by_xpath("//div[@class='input-field']//in
 input_field_code.send_keys(code_phone)
 
 time.sleep(6)
-#michael franco data peer id
-#1015336350
+
 
 #clicks on test chat with messages
-peer_chat = driver.find_element_by_xpath("//li[@data-peer-id='-1572277888']")
+peer_chat = driver.find_element_by_xpath("//li[@data-peer-id='-1304330024']")
 peer_chat.click()
 
 time.sleep(6)
@@ -104,10 +104,10 @@ top_message_element = driver.find_element_by_class_name('message')
 
 while True:
     messages = driver.find_elements_by_class_name('message')
-    time.sleep(2)
+    time.sleep(1)
 
     for i, message in enumerate(messages):
-        if message.text not in membership_check_set and len(message.text) > 20:
+        if message.text not in membership_check_set and '@' in message.text:
 
             print("This is message: ")
             print(message_count)
@@ -132,58 +132,98 @@ while True:
 
 
             #at this point, it produces a list of each line within the quotes
+
+            #Split every message.text by different separators, and take len of each one
+            #the one with the longest len, is the best one to use.
+
             string_list = re.split(', |\n|!', message.text)
 
+            print("Here is the STRING LIST")
 
-
-            print("Here is the String List")
             print(string_list)
 
+            #string_list = re.split(' |\n|!', message.text)
+            #string_list_2 = re.split('"|" |\n|!', message.text)
+
+
+
+
+
+
+
+            #print("Here is the String List")
+            #print(string_list)
+
             lines_num = len(string_list)
-            print("Here is len of String list:")
-            print(lines_num)
+            #print("Here is len of String list:")
+            #print(lines_num)
 
-            string_list_indiv=[]
-            for i, line in enumerate(string_list):
-                string_list_indiv += line.split(',')
+            string_list_indiv_0=[]
+            for line in string_list:
+                string_list_indiv_0 += line.split(',')
 
-            print(string_list_indiv)
+            string_list_indiv_1 = []
+            for line in string_list:
+                string_list_indiv_1.append(line.split())
+
+            print("Here is the string list right now: ")
+            print(string_list_indiv_1)
+
+            #find max len() between both separators
+            if len(string_list_indiv_0[0]) > len(string_list_indiv_0[1]):
+                print("The comma split used: ")
+                string_list_indiv = string_list_indiv_0
+                # removes the elements of the views and the time from the list
+                string_list_indiv.pop()
+                string_list_indiv.pop()
+            else:
+                print("The space split used: ")
+                string_list_indiv =  string_list_indiv_1
+                string_list.pop()
+                string_list.pop()
+
+
+            #print(string_list_indiv)
 
 
 
-            print("TEST LIST SLICE BELOW: ")
-            #removes the elements of the views and the time from the list
-            string_list_indiv.pop()
-            string_list_indiv.pop()
+
+
 
             row_records_list = []
             #need to get the number of rows here in order to create np array and reshape
             for i in range(0, len(string_list_indiv), len_first_line):
                 row_records_list += [string_list_indiv[i:i + len_first_line]]
 
-            print("Row Records List")
+            #print("Row Records List")
+            print("Rows List: ")
             print(row_records_list)
             len_rows = len(row_records_list)
 
             #the string without the columns headers
             #print(string_list_indiv[len_first_line:])
+            try:
+                df = pd.DataFrame(np.array(string_list_indiv[len_first_line:]).reshape(len_rows-1, len_first_line), columns=first_line_list)
+                print(first_line_list)
+                print("First line length")
+                print(len(first_line_list))
 
-            df = pd.DataFrame(np.array(string_list_indiv[len_first_line:]).reshape(len_rows-1, len_first_line), columns=first_line_list)
+                # determining the name of the file
+                file_name = 'message_' + str(message_count) + '_WillsData.xlsx'
+
+                # saving the excel
+                df.to_excel(file_name)
+            except:
+                print("An Error Occurred in Dataframe creation")
+                print("Here is the data: ")
+                print("|---------------------------------------------------|")
+                print(string_list_indiv)
 
 
 
 
 
 
-            print(first_line_list)
-            print("First line length")
-            print(len(first_line_list))
-
-            # determining the name of the file
-            file_name = 'message_'+str(message_count)+'_WillsData.xlsx'
-
-            # saving the excel
-            df.to_excel(file_name)
 
 
 
@@ -206,16 +246,12 @@ while True:
     driver.execute_script(scroll)  # execute the js scroll
     time.sleep(2)  # wait for page to load new content
 
-    if message_count > 3:
+    if message_count > 10:
         break
 
-outFile = open('dictionary.py', 'w')
-outFile.write(str(final_dictionary))
 
-print("FINAL DICTIONARY")
-print("Message Count: ")
-print(message_count)
-print(str(final_dictionary))
+
+
 
 #Log
 
